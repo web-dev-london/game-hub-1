394dbb3f6cd41df7c78d27f4bee0cfb1d6eaad12
Murod Akhmedov <murod.london@gmail.com>
2024-08-24 15:58:17 +0100

    Simplifying time calculations

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
+import ms from "ms";
 import APIClient from "../services/api-client";
 
 export interface Genre {
@@ -13,7 +14,7 @@ const apiClient = new APIClient<Genre>("/genres");
 const useGenres = () => useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.get,
-    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    staleTime: ms("24h"), // 24 hours
     initialData: genres
 })
 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import ms from "ms";
 import APIClient from "../services/api-client";
 
 export interface Platform {
@@ -14,7 +15,7 @@ const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 const usePlatforms = () => useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.get,
-    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    staleTime: ms("24h"), // 24 hours
     initialData: platforms
 })
 
